49883c1553bf0241fb27543727b418a2997a081d
samuelrolo <207416622+samuelrolo@users.noreply.github.com>
2026-02-28 16:03:09 -0500

    perf: code splitting + favicon fix
    - React.lazy code splitting: 47% reduction in initial load (2.4MB → 1.3MB)
    - Page components lazy-loaded: Results, CareerPathResults, Home, HomeEN, etc.
    - Vendor chunks: vendor-pdf (593KB), vendor-radix (79KB), vendor-motion
    - Added favicon.ico to all directories (fixes 404 errors)
    - Added favicon link tags to all index.html files
    - Module preload hints for vendor-radix and vendor-pdf

---
+++ en/cv-analyser/assets/vendor-motion-ChCp3VgX.js
@@ -0,0 +1,1 @@
+import"./vendor-radix-DCmNHa0D.js";
---
+++ hr-report/assets/users-DHX6mpJM.js
@@ -0,0 +1,26 @@
+import{c}from"./index-BMT6_UlU.js";/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=c("CircleCheck",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["path",{d:"m9 12 2 2 4-4",key:"dzmm74"}]]);/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const r=c("Compass",[["path",{d:"m16.24 7.76-1.804 5.411a2 2 0 0 1-1.265 1.265L7.76 16.24l1.804-5.411a2 2 0 0 1 1.265-1.265z",key:"9ktpf1"}],["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}]]);/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const y=c("LoaderCircle",[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]]);/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const s=c("Target",[["circle",{cx:"12",cy:"12",r:"10",key:"1mglay"}],["circle",{cx:"12",cy:"12",r:"6",key:"1vlfrh"}],["circle",{cx:"12",cy:"12",r:"2",key:"1c9p78"}]]);/**
+ * @license lucide-react v0.453.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=c("Users",[["path",{d:"M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2",key:"1yyitq"}],["circle",{cx:"9",cy:"7",r:"4",key:"nufk8"}],["path",{d:"M22 21v-2a4 4 0 0 0-3-3.87",key:"kshegd"}],["path",{d:"M16 3.13a4 4 0 0 1 0 7.75",key:"1da9ce"}]]);export{r as C,y as L,s as T,t as U,a};
