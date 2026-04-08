2862d7c4635f96df9225a913d8a578d0772c7a26
samuelrolo <207416622+samuelrolo@users.noreply.github.com>
2026-04-08 15:41:45 -0400

    fix: add missing KnowledgeHub chunk assets to cv-analyser/assets

---
+++ cv-analyser/assets/chevron-down-DA8Xj6zV.js
@@ -0,0 +1,1 @@
+import{c as o}from"./index-CYN7U8GS.js";const n=o("ChevronDown",[["path",{d:"m6 9 6 6 6-6",key:"qrunsl"}]]);export{n as C};
---
+++ cv-analyser/assets/chevron-up-CFS9edSn.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-CYN7U8GS.js";const r=e("ChevronUp",[["path",{d:"m18 15-6-6-6 6",key:"153udz"}]]);export{r as C};
