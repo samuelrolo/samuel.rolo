82f457a2a52c713054c05fd17b4178ea0f0f2004
samuelrolo <207416622+samuelrolo@users.noreply.github.com>
2026-04-13 02:39:17 -0400

    fix: refresh all static index.html with latest build hashes, fix blank pages

---
+++ apps/cv-analyser/deploy/assets/loader-circle-Csap7pAx.js
@@ -0,0 +1,1 @@
+import{c as e}from"./index-DAi4P50w.js";const r=e("LoaderCircle",[["path",{d:"M21 12a9 9 0 1 1-6.219-8.56",key:"13zald"}]]);export{r as L};
---
+++ apps/cv-analyser/deploy/assets/vendor-motion-CYZSecEV.js
@@ -0,0 +1,1 @@
+import"./vendor-radix-Bvin3Adv.js";
